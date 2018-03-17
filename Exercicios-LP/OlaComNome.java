86de892b44dee579060e6ef8b34bcde7b99691e5
PedroCarl0s <pedro-pedroc@hotmail.com>
2018-03-17 01:38:24 -0300

    remove

--- Exercicios-LP/OlaComNome.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class OlaComNome {
-	public static void main(String[] args) {
-		String nome = JOptionPane.showInputDialog("Qual seu nome ?");
-		JOptionPane.showMessageDialog(null, "Olá " + nome);
-		
-	}
-}
--- Exercicios-LP/OlaMundo.java
+++
@@ -1,8 +0,0 @@
-// Crie uma nova classe para o seu projeto chamada “OlaMundo”
-
-public class OlaMundo {
-	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
-		
-	}
-}
