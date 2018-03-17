b2a7ae39e635b010c36daddae7429c30b84ceae2
PedroCarl0s <pedro-pedroc@hotmail.com>
2018-03-17 01:42:19 -0300

    URI 1001

---
+++ URI/URI_1001.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class URI_1001 {
+	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A,B,X;
+        
+        A = input.nextInt();
+        B = input.nextInt();
+        X = (A+B);
+        System.out.printf("X = %d\n", X);
+     
+    input.close();
+	}
+}
